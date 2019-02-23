f967edbbaf53c13e01093544642acf3d4927dd58
Joshua Franklin <joshuafranklin26@gmail.com>
2019-02-23 15:02:40 -0700

    Added change nickname button.

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,19 +2,27 @@ from flask import Flask, session, request, render_template, redirect, url_for
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.secret_key = 'dev'
 socket = SocketIO(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        session['nickname'] = request.form['nickname']
+        if 'nickname' in request.form and request.form['nickname']:
+            session['nickname'] = request.form['nickname']
         return redirect(url_for('index'))
 
     if 'nickname' in session:
-        return render_template('index.html')
+        return render_template('index.html', nickname=session['nickname'])
 
     return render_template('landing.html')
+
+
+@app.route('/change')
+def change():
+    session.pop('nickname')
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
