86beab5fab4054bce88af5a26d6c1a155c400ee5
Joshua Franklin <joshuafranklin26@gmail.com>
2019-02-23 14:54:58 -0700

    Added color macros.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, session, request, render_template, redirect, url_for
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socket = SocketIO(app)
 
 
-@app.route('/')
-def index_route():
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        session['nickname'] = request.form['nickname']
+        return redirect(url_for('index'))
+
+    if 'nickname' in session:
+        return render_template('index.html')
+
     return render_template('landing.html')
 
 
