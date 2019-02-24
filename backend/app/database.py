00687b9d6f72e60a00ea22b726f9825dfeaa6e82
Harsh Deshpande <harshdeshpande@cu-biot-9-10.203.160.116.int.colorado.edu>
2019-02-23 19:03:09 -0700

    Made add and delete functions

--- backend/app/database.py
+++ backend/app/database.py
@@ -36,14 +36,22 @@ class Database:
 
         # Create the tables is needed.
         if needs_tables:
-            self.cursor.execute("CREATE TABLE users(ID int NOT NULL AUTO_INCREMENT, Username varchar(255) NOT NULL UNIQUE, Password varchar(255) NOT NULL, created_at TIMESTAMP WITH TIME ZONE MST CURRENT_TIMESTAMP, PRIMARY KEY (ID))")
-            self.cursor.execute("CREATE TABLE posts(ID int NOT NULL AUTO_INCREMENT, FOREIGN KEY (uid) REFERENCES users(uid),Title varchar(255), Content varchar(255), created_at TIMESTAMP WITH TIME ZONE MST CURRENT_TIMESTAMP, PRIMARY KEY (ID), FOREIGN KEY (parent) REFERENCES posts(parent)), domain varchar(255)")
-            self.cursor.execute("CREATE TABLE votes(ID int NOT NULL AUTO_INCREMENT, FOREIGN KEY (uid) REFERENCES users(uid), FOREIGN KEY (parent) REFERENCES posts(parent), created_at TIMESTAMP WITH TIME ZONE MST CURRENT_TIMESTAMP, PRIMARY KEY (ID)))")
+            self.cursor.execute("CREATE TABLE users(ID SERIAL PRIMARY KEY, Username varchar(255) NOT NULL UNIQUE, Password varchar(255) NOT NULL, salt int,created_at TIMESTAMP WITH TIME ZONE MST CURRENT_TIMESTAMP")
+            self.cursor.execute("CREATE TABLE posts(ID SERIAL PRIMARY KEY, FOREIGN KEY (uid) REFERENCES users(uid),Title varchar(255), Content varchar(255), FOREIGN KEY (parent) REFERENCES posts(parent)), domain varchar(255), created_at TIMESTAMP WITH TIME ZONE MST CURRENT_TIMESTAMP")
+            self.cursor.execute("CREATE TABLE votes(ID SERIAL PRIMARY KEY, FOREIGN KEY (uid) REFERENCES users(uid), FOREIGN KEY (parent) REFERENCES posts(parent), created_at TIMESTAMP WITH TIME ZONE MST CURRENT_TIMESTAMP")
             
 
         log(f'Database initialized! Name: {DATABASE_NAME}')
 
     def create_user(self, username, password):
         pw_hash, salt = generate_hash(password)
+        self.cursor.execute("INSERT INTO users(username, password, salt) VALUES (%s, %s, %s)", (username, pw_hash, salt))
+        
+    def add_comment(self, post_name, content, parent_id, user_id, domain):
+        if(parent_id == -1):
+            parent_id = None
+        self.cursor.execute("INSERT INTO posts(uid, title, content, parent, site) VALUES (%s, %s, %s, %s, %s)", (user_id, post_name, content, parent_id, domain))
 
+    def delete_comment(self, post_id):
+        self.cursor.execute("DELETE FROM posts WHERE ID=%s", (post_id))
 
