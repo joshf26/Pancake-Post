518e088ab3fc5d7c1e5716f6d660b14b253a25c7
Joshua Franklin <joshuafranklin26@gmail.com>
2019-02-24 10:26:50 -0700

    Comment window now reopens.

--- backend/app/static/js/modal.js
+++ backend/app/static/js/modal.js
@@ -11,11 +11,21 @@ function view_post(post_id) {
 
 document.addEventListener('DOMContentLoaded', function () {
     const modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals, {});
+    const m = M.Modal.init(modals, {});
 
     document.querySelectorAll('.collection-item').forEach((item) => {
         item.addEventListener('click', () => {
             view_post(item.id.split('view-post-')[1])
         });
     });
+
+    const url = new URL(window.location.href);
+    if (url.searchParams.get('post_id')) {
+        view_post(url.searchParams.get('post_id'));
+        m.forEach((modal) => {
+            if (modal['el'] === document.getElementById('view-post-modal')) {
+                modal.open();
+            }
+        });
+    }
 });
