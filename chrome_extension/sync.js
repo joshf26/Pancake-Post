2c9468a2ec45ce484eb78f7c54bccef7427dbd26
Joshua Franklin <joshuafranklin26@gmail.com>
2019-05-26 18:30:31 -0600

    add delete buttons for questions and answers

--- chrome_extension/sync.js
+++ chrome_extension/sync.js
@@ -7,19 +7,46 @@ function post_answer(answer) {
 }
 
 function delete_question(question) {
+    // TODO: This should be a cloud function, since users could decide not to delete the answers.
+    database.collection('answers').where('question', '==', database.collection('questions').doc(question)).get().then(collection => {
+        collection.forEach(document => {
+            document.ref.delete();
+        });
+    });
+
     database.collection('questions').doc(question).delete();
+}
+
+function delete_answer(answer) {
+    database.collection('answers').doc(answer).delete();
 }
 
 function setup_listeners() {
     database.collection('questions').where('domain', '==', hostname).onSnapshot(document => {
-        document.forEach(question => {
-            add_question(question.id, question.data());
+        document.docChanges().forEach(change => {
+            const question = change.doc;
 
-            database.collection('answers').where('question', '==', database.collection('questions').doc(question.id)).onSnapshot(document => {
-                document.forEach(answer => {
-                    add_answer(question.id, answer.id, answer.data());
-                });
-            })
+            if (change.type === 'added') {
+                add_question(question.id, question.data());
+
+                database.collection('answers').where('question', '==', database.collection('questions').doc(question.id)).onSnapshot(document => {
+                    document.docChanges().forEach(change => {
+                        const answer = change.doc;
+
+                        if (change.type === 'added') {
+                            add_answer(question.id, answer.id, answer.data());
+                        } else if (change.type === 'modified') {
+                            // TODO
+                        } else {
+                            remove_answer(answer.id);
+                        }
+                    });
+                })
+            } else if (change.type === 'modified') {
+                // TODO
+            } else {
+                remove_question(question.id);
+            }
         });
     });
 }
--- chrome_extension/view.js
+++ chrome_extension/view.js
@@ -71,8 +71,31 @@ function add_question(id, question) {
 function add_answer(question_id, id, answer) {
     const answer_element = document.createElement('p');
     answer_element.innerText = answer.text;
+    answer_element.id = id;
+
+    if (answer.created_by === firebase.auth().currentUser.uid) {
+        const delete_button = document.createElement('button');
+        delete_button.innerText = 'Delete';
+        delete_button.addEventListener('click', () => {
+            delete_answer(id);
+        });
+        answer_element.appendChild(delete_button);
+    }
 
     document.getElementById(question_id).appendChild(answer_element);
+}
+
+function remove_question(id) {
+    const question_element = document.getElementById(id);
+    question_element.parentElement.removeChild(question_element);
+}
+
+function remove_answer(id) {
+    const answer_element = document.getElementById(id);
+
+    // The answer element could be null if the question containing the answer was deleted.
+    if (answer_element)
+        answer_element.parentElement.removeChild(answer_element);
 }
 
 function create_question() {
@@ -81,16 +104,22 @@ function create_question() {
         created_by: firebase.auth().currentUser.uid,
         created_on: firebase.firestore.Timestamp.fromDate(new Date()),
         domain: hostname
-    })
+    });
+
+    question_textarea.value = '';
 }
 
 function create_answer(question_element) {
+    const answer_textarea = question_element.querySelector('textarea');
+
     post_answer({
-        text: question_element.querySelector('textarea').value,
+        text: answer_textarea.value,
         created_by: firebase.auth().currentUser.uid,
         created_on: firebase.firestore.Timestamp.fromDate(new Date()),
         question: database.collection('questions').doc(question_element.id)
     });
+
+    answer_textarea.value = '';
 }
 
 document.getElementById('post-question-button').addEventListener('click', create_question);
